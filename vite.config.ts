792d41f79938eb2a27fce4c354e68fb47eea2e11
zhouzhou <is-zhou@outlook.com>
2025-09-12 12:44:17 +0800

    build: 新增element-plus并设置peerDependencies

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import dts from "vite-plugin-dts" //将.vue或.ts(x)文件生成*.d.ts声明文件
 import path from 'path'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig(({ command }: { command: 'serve' | 'build'; mode: string }) => {
     if (command === 'serve') {
@@ -17,7 +20,16 @@ export default defineConfig(({ command }: { command: 'serve' | 'build'; mode: st
     }
 
     return {
-        plugins: [vue(), dts()],
+        plugins: [
+            vue(),
+            dts(),
+            AutoImport({
+                resolvers: [ElementPlusResolver()],
+            }),
+            Components({
+                resolvers: [ElementPlusResolver()],
+            }),
+        ],
         build: {
             lib: {
                 entry: path.resolve(__dirname, 'src/index.ts'),//库入口文件
@@ -25,10 +37,11 @@ export default defineConfig(({ command }: { command: 'serve' | 'build'; mode: st
                 fileName: (format) => `vue-serialize-input.${format}.js`//指定了库文件的命名规则，format会根据不同的输出格式（如es、cjs`）生成不同的文件名
             },
             rollupOptions: {
-                external: ["vue"],//配置表示 vue 是一个外部依赖，它不会被打包进最终的库中，而是期望用户在使用这个库时已经加载了 Vue。
+                external: ["vue", 'element-plus'],//配置表示 vue 是一个外部依赖，它不会被打包进最终的库中，而是期望用户在使用这个库时已经加载了 Vue。
                 output: {
                     globals: {
-                        vue: "Vue" //告诉 Rollup，当库被打包时，vue 作为外部依赖，它的全局变量名是 Vue。这个设置是为了支持 UMD 和 IIFE 格式的输出文件。
+                        vue: "Vue", //告诉 Rollup，当库被打包时，vue 作为外部依赖，它的全局变量名是 Vue。这个设置是为了支持 UMD 和 IIFE 格式的输出文件。
+                        'element-plus': 'ElementPlus'
                     }
                 }
             }
